bdc4c882a5b4a8c049425766ecac2ffb3418fb02
Adam Romański <adam.romanski@netguru.pl>
2019-03-04 20:11:54 +0100

    Create single controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,31 @@
-const note = require('./noteController');
-const twitter = require('./twitterController');
-const article = require('./articleController');
+const mongoose = require('mongoose');
+require('../models/Note');
 
-module.exports = {
-  note,
-  twitter,
-  article
+const Note = mongoose.model('notes');
+
+const note = {
+  addNote: (req, res) => {
+    const newNoteContent = {
+      title: req.body.title,
+      description: req.body.description,
+    };
+    new Note(newNoteContent)
+      .save()
+      .then(idea => console.log('Note saved:', idea))
+      .then(res.send(200));
+  },
+  getAllNotes: (req, res) => {
+    console.log('get all twitters');
+  },
+  getSingleNote: (req, res) => {
+    console.log('get single twitter');
+  },
+  updateNote: (req, res) => {
+    console.log('update single twitter');
+  },
+  deleteNote: (req, res) => {
+    console.log('delete single twitter');
+  }
 };
+
+module.exports = note;
